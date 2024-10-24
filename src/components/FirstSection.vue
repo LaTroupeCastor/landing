6ed191e8b06bb9c5a4685dd1a7522aa32928d135
Metais Bastien <bastienmetais79@gmail.com>
2024-10-24 10:11:36 +0200

    :sparkles: Ajout de la première section (pas terminé) et modification du Header

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -1,14 +1,13 @@
 <template>
   <section>
-    <div class="w-8/12 text-center">
-      <h1 class="header-1 pb-10">L’outil clé en main <br> pour la rénovation de votre bien !</h1>
-      <h4 class="poppins-light ">Vous souhaitez rénover votre bien, mais vous ne s’avez pas comment vous y prendre ? </h4>
-      <h2 class="poppins-subtitle  py-6">BEAVY EST LA SOLUTION !</h2>
-      <div class="flex justify-center">
-        <h4 class="pr-1 poppins-body-bold pb-4">Facilité, proximité, rapidité</h4>
-        <h4 class="poppins-body !font-light">sont nos maîtres mots</h4>
+    <div class="grid justify-between">
+      <img src="../assets/circle_left.svg" class="col-start-1 row-start-1" alt="Tete Logo" />
+      <div class="flex col-start-1 row-start-1">
+        <div class="header-1 p-3">
+          Rénover votre passoire thermique n’a jamais été aussi simple.
+        </div>
+        <img src="../assets/illuhero.svg" alt="Tete Logo" />
       </div>
-      <h5 class="poppins-little">Nous réalisons pour vous : votre diagnostic énergétique, la recherche des aides financières auxquelles vous êtes éligibles, et nous veillons au bon déroulement des travaux avec un contact fluide avec nos artisans partenaires. </h5>
     </div>
   </section>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <nav class="bg-background1 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav class="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
       <div class="flex flex-wrap justify-between mx-24">
         <a href="https://flowbite.com" class="flex items-center">
           <img src="../assets/tete.svg" class="mr-3 h-6 sm:h-9" alt="Tete Logo" />
