ed79e9394f4fdb59d36dce9cb34b5a25d73a2829
Metais Bastien <bastienmetais79@gmail.com>
2024-10-26 14:36:40 +0200

    feat: update FirstSection component with new design and functionality

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -1,15 +1,19 @@
 <template>
   <section>
     <div class="grid justify-between">
-      <img src="../assets/circle_left.svg" class="col-start-1 row-start-1" alt="Tete Logo" />
+      <img src="../assets/circle_top_left.svg" class="col-start-1 row-start-1" alt="Tete Logo" />
       <div class="flex col-start-1 row-start-1">
-        <div class="header-1 pl-36 pt-36">
-          Rénover votre passoire thermique n’a jamais été aussi simple.
+        <div class="pl-36 pt-36">
+          <div class="headline-large-sbold pb-9">Rénover votre <span class="text-primary">passoire thermique</span> n’a jamais été aussi simple.</div>
+          <div class="title-medium-medium pb-8">Obtenez des aides pour vos travaux et un accompagnement humain de A à Z</div>
+          <Button :cta="true">Faire une simulation en moins de 10 minutes</Button>
         </div>
-        <img  class="h-min w-2/5" src="../assets/illuhero.svg" alt="Tete Logo" />
+        <img  class="w-1/2" src="../assets/illuhero.svg" alt="Tete Logo" />
+        
       </div>
     </div>
   </section>
 </template>
 <script setup lang="ts">
+import Button from "./Button.vue";
 </script>
