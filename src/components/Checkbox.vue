9764d7e309e6a81a4b5aba279e065235f2db8a16
Metais Bastien <bastienmetais79@gmail.com>
2024-10-26 14:47:21 +0200

    finish first section

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="w-6 h-6 border-2 border-primary rounded flex items-center justify-center">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-primary" viewBox="0 0 20 20" fill="currentColor">
-      <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+  <div class="pt-10 flex">
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M5.5 10L8.5 13L14.5 7M5.8 19H14.2C15.8802 19 16.7202 19 17.362 18.673C17.9265 18.3854 18.3854 17.9265 18.673 17.362C19 16.7202 19 15.8802 19 14.2V5.8C19 4.11984 19 3.27976 18.673 2.63803C18.3854 2.07354 17.9265 1.6146 17.362 1.32698C16.7202 1 15.8802 1 14.2 1H5.8C4.11984 1 3.27976 1 2.63803 1.32698C2.07354 1.6146 1.6146 2.07354 1.32698 2.63803C1 3.27976 1 4.11984 1 5.8V14.2C1 15.8802 1 16.7202 1.32698 17.362C1.6146 17.9265 2.07354 18.3854 2.63803 18.673C3.27976 19 4.11984 19 5.8 19Z" stroke="#F1AB0E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
+    <span class="text-black body-large-medium pl-2">{{ text }}</span>
   </div>
 </template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps({
+  text: {
+    type: String,
+    default: ""
+  }
+});
+</script>
+
--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -6,13 +6,13 @@
         <div class="pl-36 pt-36">
           <div class="headline-large-sbold pb-9">Rénover votre <span class="text-primary">passoire thermique</span> n’a jamais été aussi simple.</div>
           <div class="title-medium-medium pb-8">Obtenez des aides pour vos travaux et un accompagnement humain de A à Z</div>
-          <div class="flex items-center gap-2">
-            <Button :cta="true">Faire une simulation en moins de 10 minutes</Button>
-            <Checkbox />
+          <Button :cta="true">Faire une simulation en moins de 10 minutes</Button>
+          <div class="flex">
+            <Checkbox :text="'Gratuit'" class="pr-9"/>
+            <Checkbox :text="'Sans engagement'"/>
           </div>
         </div>
         <img  class="w-1/2" src="../assets/illuhero.svg" alt="Tete Logo" />
-        
       </div>
     </div>
   </section>
