c53a95b1f9e1ce41dcb9d06032106b2b15a40599
Metais Bastien <bastienmetais79@gmail.com>
2024-10-27 19:31:07 +0100

    change header padding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav class="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-      <div class="flex flex-wrap justify-between mx-24">
+      <div class="flex flex-wrap justify-between">
         <a href="https://flowbite.com" class="flex items-center">
           <img src="../assets/tete.svg" class="mr-3 h-6 sm:h-9" alt="Tete Logo" />
         </a>
