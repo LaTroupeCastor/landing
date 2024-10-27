216549a892355a33af5ca14d30b4298203a9b3ed
Metais Bastien <bastienmetais79@gmail.com>
2024-10-27 18:40:04 +0100

    feat: Add SecondSection component and crane image

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import MainLayout from './components/layout/MainLayout.vue'
 import FirstSection from "./components/FirstSection.vue";
+import SecondSection from "./components/SecondSection.vue";
 </script>
 
 <template>
   <MainLayout>
     <FirstSection />
+    <SecondSection />
   </MainLayout>
 </template>
 
---
+++ src/components/SecondSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid justify-between px-36">
+    <div class="title-small-regular text-primary">Un accompagnement humain, des résultats concrets</div>
+    <div class="headline-medium-sbold w-3/4">Beavy, votre partenaire pour une rénovation sereine.</div>
+
+  </div>
+</template>
+<script setup lang="ts">
+</script>
