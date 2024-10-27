6d97c01d502645ce98d4f187888a180e2518cc24
Metais Bastien <bastienmetais79@gmail.com>
2024-10-27 19:22:00 +0100

    feat: Add body-large-regular class to SecondSection component

--- src/components/SecondSection.vue
+++ src/components/SecondSection.vue
@@ -4,7 +4,7 @@
     <div class="headline-medium-sbold w-[90%]">Beavy, votre partenaire pour une rénovation sereine.</div>
     <div class="flex pl-10 pt-16 gap-[20%] items-center">
       <img src="../assets/crane.png" alt="Crane" class="w-[40%] h-[95%]" />
-      <div class="flex-col w-[30%]">
+      <div class="flex-col w-[30%] body-large-regular">
         <div class="pb-4">“Chez Beavy, nous faisons plus que simplement vous mettre en relation avec des artisans qualifiés.</div>
         <div class="pb-4">Nous vous accompagnons du début à la fin de votre projet, tout en vous aidant à obtenir les aides financières auxquelles vous avez droit.”</div>
         <div class="w-[10%] h-[6px] bg-primary bg-opacity-20 my-4"></div>
