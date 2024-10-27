4e3fbb6ff195dc9c329ec39e12266eba9e330876
Metais Bastien <bastienmetais79@gmail.com>
2024-10-27 19:16:54 +0100

    feat: Adjust width and height of divider in SecondSection component

--- src/components/SecondSection.vue
+++ src/components/SecondSection.vue
@@ -7,7 +7,7 @@
       <div class="flex-col w-[30%]">
         <div class="pb-4">“Chez Beavy, nous faisons plus que simplement vous mettre en relation avec des artisans qualifiés.</div>
         <div class="pb-4">Nous vous accompagnons du début à la fin de votre projet, tout en vous aidant à obtenir les aides financières auxquelles vous avez droit.”</div>
-        <div class="w-[200px] h-[4px] bg-primary bg-opacity-20 my-4"></div>
+        <div class="w-[10%] h-[6px] bg-primary bg-opacity-20 my-4"></div>
         <div>Bastien Métais, <span class="font-bold">co-fondateur de Beavy</span></div>
       </div>
     </div>
