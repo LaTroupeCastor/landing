ddd24a48e044dfbb79154f5abdc0760a04015f0b
Metais Bastien <bastienmetais79@gmail.com>
2024-11-01 14:06:24 +0100

    feat: Add SecondSection component

---
+++ src/components/sections/SecondSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid justify-between px-36">
+    <div class="title-small-regular text-primary">Un accompagnement humain, des résultats concrets</div>
+    <div class="headline-medium-sbold w-[80%]">Beavy, votre partenaire pour une rénovation sereine.</div>
+    <div class="flex pl-10 pt-16 gap-[20%] items-center">
+      <img src="../../assets/circle_section_2.svg" alt="Circle" class="absolute transform -translate-x-1/4" />
+      <div class="w-[40%]">
+        <img src="../../assets/crane.png" alt="Crane" class="relative w-full" />
+      </div>
+      <div class="flex-col w-[30%] body-large-regular">
+        <div class="pb-4">“Chez Beavy, nous faisons plus que simplement vous mettre en relation avec des artisans qualifiés.</div>
+        <div class="pb-4">Nous vous accompagnons du début à la fin de votre projet, tout en vous aidant à obtenir les aides financières auxquelles vous avez droit.”</div>
+        <div class="w-[10%] h-[6px] bg-primary bg-opacity-20 my-4"></div>
+        <div>Bastien Métais, <span class="font-bold">co-fondateur de Beavy</span></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
