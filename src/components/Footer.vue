9ca884512b66a4e77021f908cbf4dee58b468923
Metais Bastien <bastienmetais79@gmail.com>
2025-01-08 16:08:44 +0100

    Ajout du footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-row space-x-14 bg-[#F3F5F7] py-20 px-36">
+    <img :src="CastorFooter" alt="Castor footer" />
+    <div class="flex flex-col justify-center">
+      <div class="mb-7">
+        Chez <strong>LaTroupeCastor</strong>, nous nous engageons à vous mettre en relation avec les bons professionnels
+        pour garantir la réussite de votre projet de rénovation énergétique. C'est notre priorité absolue !
+      </div>
+      <div class="flex flex-row space-x-4">
+        <img :src="Facebook" alt="Facebook" />
+        <img :src="Twitter" alt="Twitter" />
+        <img :src="Instagram" alt="Instagram" />
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <div class="title-large-sbold text-black-100 mb-4">LaTroupeCastor</div>
+      <div class="flex flex-col space-y-2">
+        <div class="body-large-regular text-black-60">Aides et primes</div>
+        <div class="body-large-regular text-black-60">Notre équipe</div>
+        <div class="body-large-regular text-black-60">Votre espace client</div>
+        <div class="body-large-regular text-black-60">Faire une simulation</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import CastorFooter from '../assets/castor_footer.svg';
+import Facebook from '../assets/facebok.svg';
+import Twitter from '../assets/twitter.svg';
+import Instagram from '../assets/insta.svg';
+</script>
