00fdc502ffa38a7b66dedfa2973359c83271fd0d
Metais Bastien <bastienmetais79@gmail.com>
2025-01-09 19:07:34 +0100

    Fix responsive header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,37 @@
 <template>
   <header class="bg-white relative z-50">
-    <nav class="border-gray-200 px-7 py-4 dark:gray-800">
-      <div class="flex flex-wrap justify-between">
-        <a href="https://flowbite.com" class="flex items-center">
-          <img src="../assets/tete.svg" class="mr-3 h-14" alt="Tete Logo" />
+    <nav class="border-gray-200 px-7 py-4">
+      <div class="flex w-full justify-between items-center">
+        <!-- Logo castor -->
+        <a href="#" class="flex items-center">
+          <img src="../assets/tete.svg" class="h-14" alt="Tete Logo" />
         </a>
-        <div class="flex items-center lg:order-2">
+
+        <!-- Menu et boutons -->
+        <div class="flex items-center gap-8">
           <!-- Menu desktop -->
-          <div class="hidden justify-between items-end w-full lg:flex lg:w-auto pr-8" id="mobile-menu-2">
-            <ul class="flex flex-col mt-4 title-small-regular lg:flex-row lg:space-x-8 lg:mt-0">
+          <div class="hidden lg:block">
+            <ul class="flex title-small-regular space-x-8">
               <li>
-                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-100 lg:p-0
- dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Aides et Primes</a>
+                <a href="#" class="hover:text-primary-100">Aides et Primes</a>
               </li>
               <li>
-                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-100 lg:p-0
- dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Notre équipe</a>
+                <a href="#" class="hover:text-primary-100">Notre équipe</a>
               </li>
             </ul>
           </div>
-          <div class="big-separator"></div>
-          <div class="flex">
-            <!-- Bouton espace client visible uniquement en desktop -->
+
+          <!-- Boutons -->
+          <div class="flex items-center gap-4">
             <div class="hidden lg:block">
               <Button>Votre espace client</Button>
-              <div class="little-separator"></div>
             </div>
             <Button :cta="true">Faire une simulation</Button>
           </div>
 
-          <!-- Menu mobile -->
-          <button @click="toggleMenu" type="button" class="inline-flex items-center p-1 sm:p-2 ml-6 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none
- focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+          <!-- Menu hamburger mobile -->
+          <button @click="toggleMenu" type="button" class="inline-flex items-center p-1 sm:p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none
+ focus:ring-2 focus:ring-gray-200">
             <span class="sr-only">Open main menu</span>
             <svg class="w-8 h-8 sm:w-10 sm:h-10" :class="{ 'hidden': isMenuOpen }" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0
