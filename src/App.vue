d3938903f2c7adc6a328c9867dfc670b6049b366
Metais Bastien <bastienmetais79@gmail.com>
2025-01-09 18:38:03 +0100

    First section responsive

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
 import MainLayout from './components/layout/MainLayout.vue'
 import FirstSection from "./components/sections/FirstSection.vue";
-import SecondSection from "./components/sections/SecondSection.vue";
-import ThirdSection from "./components/sections/ThirdSection.vue";
-import FourthSection from "./components/sections/FourthSection.vue";
-import FifthSection from "./components/sections/FifthSection.vue";
-import FormSection from "./components/sections/FormSection.vue";
 import ToastManager from "./components/ToastManager.vue";
-import Footer from "./components/Footer.vue";
 
 </script>
 
