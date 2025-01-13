99f52d171799354d6b47568867c5c0ed75933fce
Metais Bastien <bastienmetais79@gmail.com>
2025-01-13 22:06:22 +0100

    Fifth section responsive

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import ToastManager from "./components/ToastManager.vue";
 import SecondSection from "./components/sections/SecondSection.vue";
 import ThirdSection from "./components/sections/ThirdSection.vue";
 import FourthSection from "./components/sections/FourthSection.vue";
+import FifthSection from "./components/sections/FifthSection.vue";
 
 </script>
 
@@ -15,6 +16,7 @@ import FourthSection from "./components/sections/FourthSection.vue";
       <SecondSection />
       <ThirdSection />
       <FourthSection />
+      <FifthSection />
     </div>
   </MainLayout>
   <ToastManager />
--- src/components/sections/FifthSection.vue
+++ src/components/sections/FifthSection.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="section-style pb-12">
-    <div class="bg-secondary-5 p-14">
-      <p class="headline-medium-sbold pl-12 pb-14">Ils nous font confiance</p>
-      <div class="flex justify-center items-center gap-16 pb-14">
-        <TheyTrustUs />
-        <TheyTrustUs />
-        <TheyTrustUs />
+    <div class="bg-secondary-5 p-8 md:p-14">
+      <h2 class="text-center text-2xl md:text-3xl font-semibold text-primary-1 mb-10">
+        Ils nous font confiance.
+      </h2>
+
+      <div class="flex flex-col lg:flex-row gap-6 max-w-6xl mx-auto">
+        <TheyTrustUs v-for="i in 3" :key="i" />
       </div>
-      <div class="flex justify-center">
-        <Button :cta="true">Commencez votre simulation
-          maintenant</Button>
+
+      <div class="flex justify-center mt-8">
+        <Button :cta="true">Commencez votre simulation maintenant</Button>
       </div>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import TheyTrustUs from "../TheyTrustUs.vue";
 import Button from "../Button.vue";
