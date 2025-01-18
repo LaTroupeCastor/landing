97fb6a3f6385c050d813df2329308564462bc637
Metais Bastien <bastien@playmoweb.com>
2025-01-18 10:11:56 +0100

    style: Add w-fit class to RectangleWithIcon for compact width

--- src/components/RectangleWithIcon.vue
+++ src/components/RectangleWithIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-primary-10 p-3 rounded-md lg:p-5"><img :src="src" class="sm:w-5 sm:h-5" :alt="alt" /></div>
+  <div class="bg-primary-10 p-3 rounded-md lg:p-5 w-fit"><img :src="src" class="sm:w-5 sm:h-5" :alt="alt" /></div>
 </template>
 
 <script setup lang="ts">
