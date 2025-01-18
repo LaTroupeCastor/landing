4f27c5e302326a7eaf9f97dfc26191437b29552d
Metais Bastien <bastien@playmoweb.com>
2025-01-18 11:34:00 +0100

    style: Adjust footer spacing to remove extra mobile margin

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col lg:flex-row lg:space-x-14 space-y-10 lg:space-y-0 bg-[#F3F5F7] py-10 lg:py-20 px-8 lg:px-36">
+  <div class="flex flex-col lg:flex-row lg:space-x-14 space-y-8 lg:space-y-0 bg-[#F3F5F7] py-10 lg:py-20 px-8 lg:px-36">
     <img :src="CastorFooter" alt="Castor footer" class="w-48 lg:w-auto" />
-    <div class="flex flex-col justify-center space-y-4">
+    <div class="flex flex-col justify-center">
       <div class="mb-7 hidden lg:block">
         Chez <strong>LaTroupeCastor</strong>, nous nous engageons à vous mettre en relation avec les bons professionnels
         pour garantir la réussite de votre projet de rénovation énergétique. C'est notre priorité absolue !
