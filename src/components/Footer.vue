30cc5a5c0a6857387e2364ebe29bb01b4c45c5c5
Metais Bastien <bastien@playmoweb.com>
2025-01-18 11:35:38 +0100

    style: Update Footer responsive layout to use md breakpoint

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col lg:flex-row lg:space-x-14 space-y-10 lg:space-y-0 bg-[#F3F5F7] py-10 lg:py-20 px-8 lg:px-36">
-    <img :src="CastorFooter" alt="Castor footer" class="w-48 lg:w-auto" />
+  <div class="flex flex-col md:flex-row md:space-x-14 space-y-10 md:space-y-0 bg-[#F3F5F7] py-10 md:py-20 px-8 md:px-36">
+    <img :src="CastorFooter" alt="Castor footer" class="w-48 md:w-auto" />
     <div class="flex flex-col justify-center">
-      <div class="mb-7 hidden lg:block">
+      <div class="mb-7 hidden md:block">
         Chez <strong>LaTroupeCastor</strong>, nous nous engageons à vous mettre en relation avec les bons professionnels
         pour garantir la réussite de votre projet de rénovation énergétique. C'est notre priorité absolue !
       </div>
-      <div class="hidden lg:flex flex-row space-x-4">
+      <div class="hidden md:flex flex-row space-x-4">
         <img :src="Facebook" alt="Facebook" />
         <img :src="Twitter" alt="Twitter" />
         <img :src="Instagram" alt="Instagram" />
@@ -21,7 +21,7 @@
         <div class="body-large-regular text-black-60">Faire une simulation</div>
       </div>
     </div>
-    <div class="flex lg:hidden flex-row space-x-4">
+    <div class="flex md:hidden flex-row space-x-4">
       <img :src="Facebook" alt="Facebook" />
       <img :src="Twitter" alt="Twitter" />
       <img :src="Instagram" alt="Instagram" />
