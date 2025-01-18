7a34a310eadac012ffc3fec05c76a3ef53bde9b6
Metais Bastien <bastien@playmoweb.com>
2025-01-18 11:32:07 +0100

    style: Update Footer component responsive layout and visibility

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col lg:flex-row lg:space-x-14 space-y-10 lg:space-y-0 bg-[#F3F5F7] py-10 lg:py-20 px-8 lg:px-36">
     <img :src="CastorFooter" alt="Castor footer" class="w-48 lg:w-auto" />
-    <div class="flex flex-col justify-center space-y-4 lg:space-y-0">
-      <div class="mb-7">
+    <div class="flex-col justify-center space-y-4 lg:space-y-0 hidden md:flex">
+      <div class="mb-7 hidden">
         Chez <strong>LaTroupeCastor</strong>, nous nous engageons à vous mettre en relation avec les bons professionnels
         pour garantir la réussite de votre projet de rénovation énergétique. C'est notre priorité absolue !
       </div>
-      <div class="flex flex-row space-x-4">
+      <div class="flex-row space-x-4 ">
         <img :src="Facebook" alt="Facebook" />
         <img :src="Twitter" alt="Twitter" />
         <img :src="Instagram" alt="Instagram" />
@@ -20,6 +20,11 @@
         <div class="body-large-regular text-black-60">Votre espace client</div>
         <div class="body-large-regular text-black-60">Faire une simulation</div>
       </div>
+    </div>
+    <div class="flex-row space-x-4 flex md:hidden">
+      <img :src="Facebook" alt="Facebook" />
+      <img :src="Twitter" alt="Twitter" />
+      <img :src="Instagram" alt="Instagram" />
     </div>
   </div>
 </template>
