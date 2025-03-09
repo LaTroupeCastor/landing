9682df8ba8d28e7ceaa11aa62ff4ecb9aa5fdffb
prinfray <paul.rinfray@gmail.com>
2025-03-09 16:03:05 +0100

    ajout balise gtag

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
+import VueGtm from 'vue-gtm'
 import './style/style.css'
 
 const app = createApp(App)
@@ -10,4 +11,14 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+
+// Ajout de Google Tag Manager
+app.use(VueGtm, {
+  id: 'GTM-KVNNQGLJ', 
+  vueRouter: router, // Active le suivi des pages via Vue Router
+  enabled: true, // Active GTM (à desactiver en mode développement si besoin)
+  debug: false, // Passer à true pour voir les logs en console
+  loadScript: true, // Charge automatiquement le script GTM
+})
+
 app.mount('#app')
